13292a8803ecdff0cf88630160c9a1df4ceec11a
anvzhang <165081556+anvzhang@users.noreply.github.com>
2024-05-06 19:50:01 +0000

    done

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 let vegetables = ["squash", "broccoli", "carrots", "celery"];
 
+let vegetable = "cabbage";
+vegetables.unshift(vegetable);
+
+let fruits = ["apple", "banana", "orange", "grape", "kiwi"];
+
+let produce = vegetables.concat(fruits);
+console.log([...produce])
+
+produce.sort();
+console.log([...produce])
+
+produce.reverse();
+console.log([...produce])
+
+let produceString = produce.join("; ");
+console.log(produceString);
+
 
 
 
